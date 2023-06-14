c70401f6cba0ac2bc60389d63a777377ed0e9fc5
UnbrokenBond <r.christian.wilkerson@gmail.com>
2023-06-14 16:42:45 -0600

    finished the prep

---
+++ week02/scripts/bom.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.innerHTML = input.value;
+        deleteButton.innerHTML = 'X';
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        })
+        input.value = '';
+    }
+});
