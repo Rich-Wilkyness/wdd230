cb1b2411d486ba7d450986f57179fce3ceae3338
UnbrokenBond <r.christian.wilkerson@gmail.com>
2023-06-24 23:16:34 -0600

    week3 chamber assignment

--- chamber/scripts/latest-visit.js
+++ chamber/scripts/latest-visit.js
@@ -2,18 +2,19 @@ const millisecondsInDay = 86400000; //miliseconds in a day
 const currentTime = new Date();
 const visited = document.querySelector('#visited');
 
-let lastVisited = Number(window.localStorage.getItem('last-visited')) || currentTime;
+const storedValue = localStorage.getItem('last-visited') || currentTime;
+const lastVisited = storedValue ? new Date(JSON.parse(storedValue)) : currentTime;
 
 const timePassed = currentTime - lastVisited; //this is in miliseconds
+
+localStorage.setItem('last-visited', JSON.stringify(currentTime));
 
 if (timePassed === 0) {
     visited.textContent = "Welcome! Let us know if you have any questions.";
 } else if (timePassed >= millisecondsInDay) {
-    const daysSince = Math.floor(timePassed/millisecondsInDay);
+    const daysSince = (timePassed/millisecondsInDay);
     visited.textContent = `You last visited ${daysSince} day(s) ago.`;
 }
-else {
+else if (timePassed > 0 && timePassed < millisecondsInDay) {
     visited.textContent = 'Back so soon! Awesome!';
 }
-
-localStorage.setItem('last-visited', currentTime);
