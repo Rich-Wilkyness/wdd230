76d9aa90117ca74d45534a6b04106208d5453506
UnbrokenBond <r.christian.wilkerson@gmail.com>
2023-07-01 16:28:00 -0600

    completed join us form

---
+++ chamber/scripts/menu-keyboard.js
@@ -0,0 +1,24 @@
+const menuButton = document.getElementById('menu');
+const menuLinks = document.getElementById('menu-links');
+
+menuButton.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter' || event.key === ' ') {
+    event.preventDefault();
+    toggleMenu();
+  }
+});
+menuButton.addEventListener('click', function () {
+    toggleMenu();
+  });
+
+function toggleMenu() {
+  const isOpen = menuButton.getAttribute('aria-expanded') === 'true';
+
+  if (isOpen) {
+    menuButton.setAttribute('aria-expanded', 'false');
+    menuLinks.classList.remove('open');
+  } else {
+    menuButton.setAttribute('aria-expanded', 'true');
+    menuLinks.classList.add('open');
+  }
+}
---
+++ chamber/scripts/timestamp.js
@@ -0,0 +1,3 @@
+const currentTimestamp = new Date().toISOString();
+  
+document.getElementById("timestamp").value = currentTimestamp;
