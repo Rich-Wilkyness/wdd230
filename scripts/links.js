cf229358b5cf57b4427b4034391259429d050895
UnbrokenBond <r.christian.wilkerson@gmail.com>
2023-07-06 12:28:06 -0600

    trying to get this to work

--- scripts/links.js
+++ scripts/links.js
@@ -1,18 +1,39 @@
 const baseURL = "https://rich-wilkyness.github.io/wdd230/";
-
 const linksURL = "https://rich-wilkyness.github.io/wdd230/data/links.json";
+const weekElement = document.getElementById("learning-activities");
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        displayLinks(data);
+    try {
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayLinks(data);
+        } else {
+            throw new Error("Failed to fetch links");
+        }
+    } catch (error) {
+        console.error(error);
     }
 }
 
-function displayLinks(data) {
+function displayLinks(weeks) {
+    weeks.forEach((week) => {
+        const weekHeader = document.createElement("h2");
+        weekHeader.textContent = week.week;
+        weekElement.appendChild(weekHeader);
+        const weekList = document.createElement("ul");
 
+        week.links.forEach((link) => {
+            const liElement = document.createElement("li");
+            const aElement = document.createElement("a");
+            aElement.href = link.url;
+            aElement.textContent = link.title;
+            liElement.appendChild(aElement);
+            weekList.appendChild(liElement);
+        });
+        weekElement.appendChild(weekList);
+    });
 }
 
 getLinks();
